3418db28404df1ccdd4f7af14ab7b58281d39b5b
Abd Alrifai <abd.alrifai963@gmail.com>
2024-08-01 15:28:39 +0300

    temporary name
    in future I will change it

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:learning_system/src/features/auth/welcome/welcome.dart';
 import 'package:learning_system/src/features/user/prsentation/profile_user.dart';
+=======
+import 'package:learning_system/src/features/user/presentation/profile.dart';
+//import 'package:learning_system/src/feautres/auth/welcome/welcome.dart';
+>>>>>>> Stashed changes
 
 class EduBridgeApp extends StatelessWidget {
   const EduBridgeApp({super.key});
@@ -9,7 +14,11 @@ class EduBridgeApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+<<<<<<< Updated upstream
       home: ProfileUser(),
+=======
+      home: ProfileUserPage()
+>>>>>>> Stashed changes
     );
   }
 }
